b7108a823f0b9df76c05dce33fd0eb5497507696
Kedar Agate <kedar.agate@gmail.com>
2023-12-17 01:55:22 +0530

    custom error messages for empty fields in step 1 developed

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 let personalInfo = document.getElementById("step-1-form");
 
-personalInfo.addEventListener("submit", () => {
-  console.log(personalInfo);
+personalInfo.querySelectorAll("input").forEach((field) => {
+  field.addEventListener("blur", () => {
+    if (!field.value) {
+      const errorMessage = document.createElement("span");
+      errorMessage.className = "required-field-message";
+
+      errorMessage.textContent = "This field is required.";
+
+      if (field.nextElementSibling == null) {
+        field.parentNode.appendChild(errorMessage);
+      }
+    }
+  });
+});
+
+personalInfo.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let data = new FormData(personalInfo);
+  let userData = Object.fromEntries(data);
+  console.log(userData);
 });
