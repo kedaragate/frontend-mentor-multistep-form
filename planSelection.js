fc1cc1549224b240458bdb57e03ec2fdec13970e
Kedar Agate <kedar.agate@gmail.com>
2023-12-21 22:06:10 +0530

    step-2-toggle-switch completed, go-back link styling is remaining

--- planSelection.js
+++ planSelection.js
@@ -1,3 +1,14 @@
 let personalInfo = JSON.parse(sessionStorage.getItem("personalInfo"));
+const toggleSwitch = document.getElementById("plan-toggle-switch");
 
+console.log(toggleSwitch.getAttribute);
 console.log(personalInfo);
+let planDuration = "";
+
+toggleSwitch.addEventListener("click", (e) => {
+  if (e.target.checked) {
+    toggleSwitch.value = "yearly";
+  } else {
+    toggleSwitch.value = "monthly";
+  }
+});
